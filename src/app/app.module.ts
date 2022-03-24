d0c1a4131c98ea46f15276bfb6291b923ab4ee7e
Vitor Melo <vitormelo9913@gmail.com>
2022-03-24 20:13:38 -0300

    @vitor-pm [Task: Add login/register/footer/menu]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RegisterComponent } from './register/register.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RegisterComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
